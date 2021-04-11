8d0a4aff5eed3acd50d9f0481103332a0616b3c2
Yaseen Al-Hashimi <55101943+Nitronoised@users.noreply.github.com>
2021-04-11 11:08:37 +0200

    fix : add solution

--- src/08-remove-from-list.js
+++ src/08-remove-from-list.js
@@ -7,7 +7,7 @@
  * @return {List}
  *
  * @example
- * For l = [3, 1, 2, 3, 4, 5] and k = 3,
+ * For l = [3, 1, 2, 3, 4, 5] and l = 3,
  * the output should be [1, 2, 4, 5]
  *
  * Singly - linked lists are already defined with this interface
@@ -17,8 +17,29 @@
  * }
  */
 
-function removeKFromList(/* l, k */) {
-  throw new Error('Not implemented');
+function removeKFromList(l, k) {
+  let lCopy = { ...l };
+  let head = lCopy;
+
+  // Check first item
+  if (head.value === k) {
+    head.value = null;
+    head = head.next;
+    lCopy = head;
+  }
+
+  while (head.next) {
+    if (head.next.value === k) {
+      // Handle last item
+      if (head.next.next === null) {
+        head.next = null;
+      } else {
+        head.next = head.next.next;
+      }
+    }
+    head = head.next;
+  }
+  return lCopy;
 }
 
 module.exports = removeKFromList;
