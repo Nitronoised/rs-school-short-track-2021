7eca134ab386c7f83da455dcd84c0ae34404f0b6
Yaseen Al-Hashimi <55101943+Nitronoised@users.noreply.github.com>
2021-04-11 11:13:07 +0200

    fix : add solution

--- src/14-queue.js
+++ src/14-queue.js
@@ -1,4 +1,4 @@
-// const ListNode = require('../extensions/list-node');
+const ListNode = require('../extensions/list-node');
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
  *
@@ -10,18 +10,41 @@
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  *
  */
-
 class Queue {
   get size() {
-    throw new Error('Not implemented');
+    let counter = 0;
+    let head = this.queue;
+    while (head) {
+      head = head.next;
+      counter++;
+    }
+    return counter;
   }
 
-  enqueue(/* element */) {
-    throw new Error('Not implemented');
+  enqueue(element) {
+    const node = new ListNode(element);
+    node.value = element;
+    if (this.queue) {
+      node.next = this.queue;
+    }
+    this.queue = node;
   }
 
   dequeue() {
-    throw new Error('Not implemented');
+    let head = this.queue;
+
+    if (this.size === 1) {
+      const element = head.value;
+      this.queue = null;
+      return element;
+    }
+
+    while (head.next.next) head = head.next;
+
+    const element = head.next.value;
+    head.next = null;
+
+    return element;
   }
 }
 
