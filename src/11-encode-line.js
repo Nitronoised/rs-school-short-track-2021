21211a66d4bccd03c3971024b798762ac2984b76
Yaseen Al-Hashimi <55101943+Nitronoised@users.noreply.github.com>
2021-04-11 11:10:14 +0200

    fix : add solution

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,30 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let encodedStr = '';
+  let seq = [];
+
+  const isNextSame = (string, i) => string[i] === string[i + 1];
+  const addSeq = (sequence, string) => {
+    let stringLocal = string;
+    if (sequence.length === 1) {
+      stringLocal += sequence[0];
+    } else {
+      stringLocal += `${sequence.length}${sequence[0]}`;
+    }
+    return stringLocal;
+  };
+
+  for (let i = 0; i < str.length; i++) {
+    seq.push(str[i]);
+    if (!isNextSame(str, i)) {
+      encodedStr = addSeq(seq, encodedStr);
+      seq = [];
+    }
+  }
+
+  return encodedStr;
 }
 
 module.exports = encodeLine;
