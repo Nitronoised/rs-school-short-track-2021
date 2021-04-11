ce4b058d3794ab12854cd45e2f6d0c6e83194a71
Yaseen Al-Hashimi <55101943+Nitronoised@users.noreply.github.com>
2021-04-11 11:04:53 +0200

    fix : add solution

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,35 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const checks = {
+    checkUp: (row, col) => matrix[row - 1][col] === true,
+    checkDown: (row, col) => matrix[row + 1][col] === true,
+    checkLeft: (row, col) => matrix[row][col - 1] === true,
+    checkRight: (row, col) => matrix[row][col + 1] === true,
+    checkUpRight: (row, col) => matrix[row - 1][col + 1] === true,
+    checkUpLeft: (row, col) => matrix[row - 1][col - 1] === true,
+    checkDownRight: (row, col) => matrix[row + 1][col + 1] === true,
+    checkDownLeft: (row, col) => matrix[row + 1][col - 1] === true,
+  };
+
+  const getMinesAmount = (row, col) => Object.values(checks).reduce((mines, checkFunc) => {
+    let minesLocal = mines;
+    try {
+      if (checkFunc(row, col)) minesLocal++;
+    } catch (err) {
+      return minesLocal;
+    }
+    return minesLocal;
+  }, 0);
+
+  return matrix.reduce((finalArr, row, rowIndex) => {
+    finalArr.push(row.reduce((rowArr, _, colIndex) => {
+      rowArr.push(getMinesAmount(rowIndex, colIndex));
+      return rowArr;
+    }, []));
+    return finalArr;
+  }, []);
 }
 
 module.exports = minesweeper;
